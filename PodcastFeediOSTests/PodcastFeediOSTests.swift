1d022fb29b7c78618f2af11bbde0e7ae9e114270
Nav <navdeep.rana@heyhub.com>
2023-04-26 19:23:31 +0530

    `PodcastsListViewController` calls loader on `viewDidLoad`

--- PodcastFeediOSTests/PodcastFeediOSTests.swift
+++ PodcastFeediOSTests/PodcastFeediOSTests.swift
@@ -6,9 +6,41 @@
 //
 
 import XCTest
-@testable import PodcastFeediOS
-
+import UIKit
+class PodcastsListViewController: UIViewController{
+    private var loader: PodcastFeediOSTests.LoaderSpy?
+   convenience init(loader: PodcastFeediOSTests.LoaderSpy){
+       self.init()
+       self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        loader?.loadCallCount += 1
+    }
+}
 final class PodcastFeediOSTests: XCTestCase {
 
+    func test_init_instanceDoesnotCallPodcastLoaderOnCreation(){
+        let loader = LoaderSpy()
+        _ = PodcastsListViewController(loader: loader)
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func test_viewDidLoad_invokesLoader(){
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
+    
+    func makeSUT() -> (PodcastsListViewController, LoaderSpy){
+        let loader = LoaderSpy()
+        let sut = PodcastsListViewController(loader: loader)
+        return (sut, loader)
+    }
+    class LoaderSpy{
+        var loadCallCount = 0
+        
+    }
    
 }
